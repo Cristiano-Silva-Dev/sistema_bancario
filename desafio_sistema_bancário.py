73f43f0b54ff7c80fa23e5c8071b920f2813382d
Cristiano-Silva-Dev <102561798+Cristiano-Silva-Dev@users.noreply.github.com>
2023-08-27 21:14:37 -0300

    Update desafio_sistema_bancário.py

--- desafio_sistema_bancário.py
+++ desafio_sistema_bancário.py
@@ -1,17 +1,17 @@
 menu = '''
 ******** MENU ********
+
     [1] DEPOSITAR
     [2] SACAR
     [3] EXTRATO
     [4] SAIR
+
+======================
 '''
 saldo = 0
 limite_saque = 500
-limite_saque_diário = 1500
 limite_saques = 3
 extrato = ''
-extrato_depósitos = 0
-extrato_saques = 0
 
 while True:
     print(menu)
@@ -22,27 +22,26 @@ while True:
             print('VALOR INVÁLIDO!')
         else:
             saldo += depósito
-            extrato_depósitos += depósito
+            extrato += f'DEPÓSITO: R$ {depósito:.2f}\n'
     elif opção == '2':
         saque = float(input('VALOR DO SAQUE: R$ '))
         if saque > saldo:
             print('NÃO É POSSÍVEL REALIZAR O SAQUE, SALDO INSUFICIENTE.')
-        elif saque > limite_saque_diário:
-            print('VALOR DO SAQUE SUPERIOR AO LIMITE DIÁRIO!')
+        elif saque < 0:
+            print('VALOR INVÁLIDO!')
         elif saque > limite_saque:
             print('VALOR DO SAQUE SUPERIOR AO LIMITE DE R$ 500.00, ENTRE COM UM NOVO VALOR.')
         elif limite_saques == 0:
             print('VOCÊ ATINGIU O LIMITE DE SAQUES DIÁRIO!')
         else:
             saldo -= saque
-            extrato_saques += saque
-            limite_saque_diário -= saque
+            extrato += f'SAQUE: R$ {saque:.2f}\n'
             limite_saques -= 1
     elif opção == '3':
         print('******** EXTRATO ********')
-        print(f'DEPÓSITOS: R$ {extrato_depósitos:,.2f}')
-        print(f'SAQUES: R$ {extrato_saques:,.2f}')
-        print(f'SALDO: R$ {saldo:,.2f}')
+        print(extrato)
+        print(f'SALDO: R$ {saldo:.2f}')
+        print('*************************')
     elif opção == '4':
         break
     else:
